edb336015f968ae3a5e28ead3af9db99b91ddd4e
24149063-sys <24149063@drngpit.ac.in>
2026-02-05 08:35:58 +0530

    Create 2575-MinimumCutsToDivideACircle

---
+++ 2575-MinimumCutsToDivideACircle/2575-MinimumCutsToDivideACircle.java
@@ -0,0 +1,10 @@
+// Last updated: 05/02/2026, 08:35:55
+class Solution {
+    public int numberOfCuts(int n) {
+        if(n==1) return 0;
+        if(n%2==0){
+            return n/2;
+        }
+        return n;
+    }
+}
