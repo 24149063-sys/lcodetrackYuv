f12525348c025f09831ec956108722ef7180deae
24149063-sys <24149063@drngpit.ac.in>
2026-02-05 08:36:21 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.java
@@ -0,0 +1,18 @@
+// Last updated: 05/02/2026, 08:36:18
+class Solution {
+    public int addDigits(int num) {
+        int sum=0;
+        while(num>=10)
+        {
+            sum=0;
+            while(num>0)
+            {
+                int temp= num%10;
+                sum+=temp;
+                num/=10;
+            }
+            num=sum;
+        }
+        return num;
+    }
+}
