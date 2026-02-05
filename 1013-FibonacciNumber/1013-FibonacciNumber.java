6ba18f325694d3127a888edadc885c2a3848b794
24149063-sys <24149063@drngpit.ac.in>
2026-02-05 08:36:13 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,15 @@
+// Last updated: 05/02/2026, 08:36:10
+class Solution {
+    public int fib(int n) {
+        int prev1=0,prev2=1,curr=0;
+        if(n<=1)
+        return n;
+        for(int i=2;i<=n;i++)
+        {
+            curr=prev1+prev2;
+            prev1=prev2;
+            prev2=curr;
+        }
+        return curr;
+    }
+}
